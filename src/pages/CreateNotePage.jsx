81a9deb031f21def6d24792cb48d8402b743692c
Lautaro-R-collins <lautarorodriguezcollins@gmail.com>
2025-10-05 00:51:53 -0300

    add page

--- src/pages/CreateNotePage.jsx
+++ src/pages/CreateNotePage.jsx
@@ -1,33 +1,25 @@
 import NoteForm from "../components/NoteForm";
-import axios from "axios";
+import api from "../api/axiosConfig.js"; // <-- usa esta instancia
 import { toast } from "react-toastify";
 import { useNavigate } from "react-router-dom";
 
-const apiUrl = import.meta.env.VITE_API_URL;
-
-export const CreateNotePage = () => {
+const CreateNotePage = () => {
   const navigate = useNavigate();
 
   const Notacreate = async (note) => {
     try {
-      await axios.post(`${apiUrl}/api/notes`, note).then((res) => {
-        if (res.status != 201) {
-          throw new Error("Error al crear una nota");
-        }
-        toast.success("Nota creada!", {
-          position: "bottom-center",
-          autoClose: 3000,
-        });
-        navigate("/")
-      });
+      const res = await api.post("/api/notes", note);
+      toast.success(`Nota creada: ${res.data.title}`);
+      navigate("/");
     } catch (error) {
       console.error("Error al crear la nota:", error);
+      toast.error(error.response?.data?.message || "Error al crear la nota");
     }
   };
 
   return (
     <div>
-      <h1 className="text-center mt-4 text-2xl font-bold">Crear Nueva tarea</h1>
+      <h1 className="text-center mt-4 text-2xl font-bold">Crear Nueva Nota</h1>
       <NoteForm
         onSubmit={Notacreate}
         initialDate={{ title: "", content: "" }}

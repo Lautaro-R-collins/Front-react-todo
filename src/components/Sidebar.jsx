1274c5387790a4561296c431dc93a7e48b8958db
Lautaro-R-collins <lautarorodriguezcollins@gmail.com>
2025-10-05 00:51:24 -0300

    add component sidebar

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,10 @@
+// Sidebar.jsx
+const Sidebar = () => {
+  return (
+    <div className="fixed top-16 left-0 w-64 h-screen bg-gray-100 z-40">
+      {/* Por ahora vacía */}
+    </div>
+  );
+};
+
+export default Sidebar

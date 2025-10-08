347dc981c0da291100331aeee0574193767859c5
Lautaro-R-collins <lautarorodriguezcollins@gmail.com>
2025-10-08 01:54:33 -0300

    actualizacion - app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import HomePage from "./pages/HomePage.jsx";
-import CreateNotePage from "./pages/CreateNotePage.jsx";
-import EditNotePage from "./pages/EditNotePage.jsx";
 import LoginPage from "./pages/LoginPage.jsx";
 import RegisterPage from "./pages/RegisterPage.jsx";
 import ProtectedRoute from "./components/ProtectedRoute.jsx";
@@ -14,7 +12,6 @@ function App() {
   return (
     <>
       <Routes>
-        {/* 👇 si hay user → Home, si no → Registro */}
         <Route
           path="/"
           element={user ? <Navigate to="/home" /> : <RegisterPage />}
@@ -30,28 +27,10 @@ function App() {
         />
 
         <Route
-          path="/createNote"
-          element={
-            <ProtectedRoute>
-              <CreateNotePage />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="/editNote/:id"
-          element={
-            <ProtectedRoute>
-              <EditNotePage />
-            </ProtectedRoute>
-          }
-        />
-
-        {/* 👇 si ya está logueado, que no entre otra vez a login/registro */}
-        <Route
           path="/login"
           element={user ? <Navigate to="/home" /> : <LoginPage />}
         />
+        
         <Route
           path="/register"
           element={user ? <Navigate to="/home" /> : <RegisterPage />}

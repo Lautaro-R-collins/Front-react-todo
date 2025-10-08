4fbd4ff04678db14e6d6504e23137be1be8d81a8
Lautaro-R-collins <lautarorodriguezcollins@gmail.com>
2025-10-08 01:54:48 -0300

    actualizar cart

--- src/components/CartNote.jsx
+++ src/components/CartNote.jsx
@@ -1,24 +1,30 @@
 import { FaPenToSquare } from "react-icons/fa6";
 import { FaTrashAlt } from "react-icons/fa";
-import { useNavigate } from "react-router-dom";
 
-export const CartNote = ({ title, content, id, date, onDelete }) => {
-  const navigate = useNavigate();
-
+export const CartNote = ({ title, content, id, date, onDelete, onEdit }) => {
   return (
-    <div className="card bg-base-300 w-full shadow-sm">
-      <div className="card-body">
-        <h2 className="card-title font-bold lg:text-2xl">{title}</h2>
-        <p className="text-amber-50">{content}</p>
-        <div className="flex justify-between mt-6">
-          <time dateTime={date}>{date}</time>
-          <div className="flex gap-4">
+    <div className="card bg-base-300 shadow-md h-56 min-w-[300px]"> 
+      <div className="card-body flex flex-col justify-between">
+        <div>
+          <h2 className="card-title font-bold text-lg lg:text-xl line-clamp-1">
+            {title}
+          </h2>
+          <p className="text-sm lg:text-base text-gray-200 mt-2 line-clamp-3">
+            {content}
+          </p>
+        </div>
+
+        <div className="flex justify-between items-center mt-3 text-sm">
+          <time dateTime={date} className="text-gray-400">
+            {date}
+          </time>
+          <div className="flex gap-4 text-lg">
             <FaPenToSquare
-              className="cursor-pointer text-white hover:text-accent text-xl"
-              onClick={() => navigate(`/editNote/${id}`)} 
+              className="cursor-pointer text-white hover:text-accent"
+              onClick={() => onEdit({ _id: id, title, content })}
             />
             <FaTrashAlt
-              className="cursor-pointer text-white hover:text-red-400 text-xl"
+              className="cursor-pointer text-white hover:text-red-400"
               onClick={() => onDelete(id)}
             />
           </div>

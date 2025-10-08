ff99636e242048d7075839f0acbda79e1631d180
Lautaro-R-collins <lautarorodriguezcollins@gmail.com>
2025-10-08 18:37:18 -0300

    cambios estilos - Form

--- src/components/NoteForm.jsx
+++ src/components/NoteForm.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
+import { AiOutlineClose } from "react-icons/ai";
 
-export const NoteForm = ({ onSubmit, initialDate }) => {
+export const NoteForm = ({ onSubmit, initialDate, closeModal }) => {
   const [note, setNotes] = useState(initialDate || { title: "", content: "" });
 
   useEffect(() => {
@@ -20,35 +21,46 @@ export const NoteForm = ({ onSubmit, initialDate }) => {
   };
 
   return (
-    <div className="px-4">
-      <form
-        onSubmit={noteSubmit}
-        className=" bg-base-300 rounded-2xl p-6 m-4 max-w-4xl mx-auto"
-      >
-        <input
-          type="text"
-          placeholder="Title"
-          id="title"
-          name="title"
-          value={note.title}
-          onChange={noteChange}
-          className="block w-full mb-8 input lg:input-lg"
-          required
-        />
+    <form
+      onSubmit={noteSubmit}
+      className="bg-base-300 rounded-2xl p-6 m-4 max-w-4xl mx-auto"
+    >
+      <div className="flex justify-between items-center mb-6">
+        <h2 className="text-xl font-bold">
+          {initialDate?._id ? "Editar Nota" : "Nueva Nota"}
+        </h2>
+        <button
+          type="button"
+          className="text-gray-600 hover:text-red-500 transition text-2xl cursor-pointer"
+          onClick={closeModal}
+        >
+          <AiOutlineClose />
+        </button>
+      </div>
 
-        <textarea
-          className="textarea textarea-bordered w-full mb-8"
-          name="content"
-          id="content"
-          placeholder="Descripción de la tarea"
-          value={note.content}
-          onChange={noteChange}
-          required
-        ></textarea>
+      <input
+        type="text"
+        placeholder="Título"
+        id="title"
+        name="title"
+        value={note.title}
+        onChange={noteChange}
+        className="block w-full mb-8 input lg:input-lg"
+        required
+      />
 
-        <button className="btn btn-soft btn-primary">Guardar</button>
-      </form>
-    </div>
+      <textarea
+        className="textarea textarea-bordered w-full mb-8"
+        name="content"
+        id="content"
+        placeholder="Descripción de la tarea"
+        value={note.content}
+        onChange={noteChange}
+        required
+      ></textarea>
+
+      <button className="btn btn-soft btn-primary w-full">Guardar</button>
+    </form>
   );
 };
 

4387ed05fb366a8332925ef9aa6aab5ca617bb21
Lautaro-R-collins <lautarorodriguezcollins@gmail.com>
2025-12-24 09:53:10 -0300

    Refactor - Formate Date

--- src/utils/FormatDate.jsx
+++ src/utils/FormatDate.jsx
@@ -1,9 +1,9 @@
-import { format } from "date-fns"
+import { format } from "date-fns";
 import { es } from "date-fns/locale";
 
 const formatData = (isoString) => {
-    const date = new Date(isoString)
-    return format(date, "dd 'de' MMM 'de' yyyy", {locale: es} )
-}
+  const date = new Date(isoString);
+  return format(date, "dd 'de' MMM 'de' yyyy", { locale: es });
+};
 
-export default formatData
+export default formatData;
